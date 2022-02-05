a77ef452cba331f6cd643175da28b894788815e1
Dan Sato <s19c1054br@s.chibakoudai.jp>
2022-02-05 12:16:57 +0900

    initial commit

---
+++ scripts/video_capture.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import param
+
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) 
+
+while(True):
+    ret, frame = capture.read()
+    #frame = calibrate(frame)
+    frame = cv2.resize(frame, (1920, 1080))
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
+
