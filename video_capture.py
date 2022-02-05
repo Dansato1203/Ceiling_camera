352c6c2079311ed48d2afaad982392bc68ce0c3c
Dan Sato <s19C1054bu@s.chibakoudai.jp>
2022-02-05 12:23:07 +0900

    mv scripts

--- video_capture.py
+++ video_capture.py
@@ -8,7 +8,6 @@ capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 while(True):
     ret, frame = capture.read()
-    #frame = calibrate(frame)
     frame = cv2.resize(frame, (1920, 1080))
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
