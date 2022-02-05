aa07c4aadb278ba61afdaee2c07c5cd2577d3e5e
Dan Sato <s19C1054bu@s.chibakoudai.jp>
2022-02-05 12:39:28 +0900

    add save image

--- video_capture.py
+++ video_capture.py
@@ -1,15 +1,21 @@
 import cv2
 import numpy as np
 import param
+import os
+import date
+
+date = datetime.datetime.now().strftime('%y%m%d')
+dir_name = date + str('-image')
 
 capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) 
+i = 0
 
 while(True):
     ret, frame = capture.read()
-    frame = cv2.resize(frame, (1920, 1080))
     cv2.imshow('frame',frame)
+    cv2.imwrite(dir_name + i.zfill(5) + '.jpg', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
