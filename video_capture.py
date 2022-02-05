c51075f33488e81e820de0363e44959422d3aed0
Dan Sato <s19c1054br@s.chibakoudai.jp>
2022-02-05 12:43:38 +0900

    bug fix

--- video_capture.py
+++ video_capture.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
-import param
 import os
-import date
+import datetime
 
 date = datetime.datetime.now().strftime('%y%m%d')
 dir_name = date + str('-image')
@@ -15,7 +14,8 @@ i = 0
 while(True):
     ret, frame = capture.read()
     cv2.imshow('frame',frame)
-    cv2.imwrite(dir_name + i.zfill(5) + '.jpg', frame)
+    cv2.imwrite(dir_name + str(i).zfill(5) + '.jpg', frame)
+    i = i + 1
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
